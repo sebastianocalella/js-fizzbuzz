594585eedb96cdc11f2e1910a28aeadf86f6d6f2
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-23 15:05:40 +0100

    implement if condition fix and introducing color

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ for(let i=0; i<100; i++){
     if (i==0){
         console.log(i+1);
         a=i+1;
-    } else if (i%3==0 && i%5==0){
+    } else if ((i+1)%3==0 && (i+1)%5==0){
         console.log('FizzBuzz');
         a='FizzBuzz';
+        document.getElementById(`${i}`).style.color = "blue";
     } else if ((i + 1)%3==0 && (i + 1)%5!=0){
         console.log('Fizz');
         a='Fizz';
@@ -17,5 +18,5 @@ for(let i=0; i<100; i++){
         console.log(i + 1);
         a=i+1;
     }
-    document.getElementById('main').insertAdjacentHTML('beforebegin', `<div> nella riga ${i + 1} appare il valore: ${a}</div>`);
+    document.getElementById('main').insertAdjacentHTML('beforebegin', `<div id="${i+1}"> nella riga ${i + 1} appare il valore: ${a}</div>`);
 }
