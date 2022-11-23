c197ae3a66454abf221a9db95c12a67103581afc
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-23 15:12:44 +0100

    string color implemented

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ for(let i=0; i<100; i++){
     } else if ((i+1)%3==0 && (i+1)%5==0){
         console.log('FizzBuzz');
         a='FizzBuzz';
-        document.getElementById(`${i}`).style.color = "blue";
     } else if ((i + 1)%3==0 && (i + 1)%5!=0){
         console.log('Fizz');
         a='Fizz';
@@ -18,5 +17,23 @@ for(let i=0; i<100; i++){
         console.log(i + 1);
         a=i+1;
     }
+    
     document.getElementById('main').insertAdjacentHTML('beforebegin', `<div id="${i+1}"> nella riga ${i + 1} appare il valore: ${a}</div>`);
+
+    if (i==0){
+        document.getElementById(`${i + 1}`).style.color = "black";
+
+    } else if ((i+1)%3==0 && (i+1)%5==0){
+        document.getElementById(`${i + 1}`).style.color = "blue";
+
+    } else if ((i + 1)%3==0 && (i + 1)%5!=0){
+        document.getElementById(`${i + 1}`).style.color = "red";
+
+    } else if ((i + 1)%3!=0 && (i + 1)%5==0){
+        document.getElementById(`${i + 1}`).style.color = "green";
+
+    } else{
+        document.getElementById(`${i + 1}`).style.color = "black";
+    }
+
 }
