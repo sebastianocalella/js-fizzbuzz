a28bca248259214e3ec3166247fd50f07f1d3aa5
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-23 14:41:42 +0100

    html resultes printed by js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 for(let i=0; i<100; i++){
+    let a;
     if (i==0){
         console.log(i+1);
+        a=i+1;
     } else if (i%3==0 && i%5==0){
         console.log('FizzBuzz');
+        a='FizzBuzz';
     } else if ((i + 1)%3==0 && (i + 1)%5!=0){
         console.log('Fizz');
+        a='Fizz';
     } else if ((i + 1)%3!=0 && (i + 1)%5==0){
         console.log('Buzz');
+        a='Buzz';
     }
     else{
         console.log(i + 1);
+        a=i+1;
     }
+    document.getElementById('main').insertAdjacentHTML('beforebegin', `<div> nella riga ${i + 1} appare il valore: ${a}</div>`);
 }
