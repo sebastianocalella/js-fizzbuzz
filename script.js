16d70a6397733659d7ffef0dbe188dc380f3ac7c
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-24 08:24:00 +0100

    code cleaning

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-for(let i=0; i<100; i++){
+for(let i=1; i<=100; i++){
     let a;
     let color;
-    if (i==0){
-        console.log(i+1);
-        a=i+1;
-        color = 'black';
-    } else if ((i+1)%3==0 && (i+1)%5==0){
+    if (i%3==0 && i%5==0){
         console.log('FizzBuzz');
         a='FizzBuzz';
         color='blue';
-    } else if ((i + 1)%3==0 && (i + 1)%5!=0){
+    } else if (i%3==0 && i%5!=0){
         console.log('Fizz');
         a='Fizz';
         color='red';
-    } else if ((i + 1)%3!=0 && (i + 1)%5==0){
+    } else if (i%3!=0 && i%5==0){
         console.log('Buzz');
         a='Buzz';
         color='green';
     }
     else{
-        console.log(i + 1);
-        a=i+1;
+        console.log(i);
+        a=i;
         color='black'
     }
 
-    document.getElementById('main').insertAdjacentHTML('beforebegin', `<div class="${color}"> nella riga ${i + 1} appare il valore: ${a}</div>`);
+    document.getElementById('main').insertAdjacentHTML('beforebegin', `<div class="${color}"> nella riga ${i} appare il valore: ${a}</div>`);
 }
